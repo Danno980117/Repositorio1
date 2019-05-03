458d60d5035e9ff951aaba4bec9cb5ea12443b01
Huarseral <huarseral@hotmail.com>
2019-05-02 20:15:24 -0500

    index2

--- index2.php
+++ index2.php
@@ -1,9 +1,15 @@
 <form>
 	<input type="text" name="cadena"/>
+	<input type="text" name="url"/>
 	<button type="submit">Contar</button>
 </form>
 
 <?php
 	extract($_GET);
 	echo strlen($cadena);
+	$data=explode("-", $url);
+
+	foreach ($data as $key => $value) {
+		echo $value, "<br />";
+	}
 ?>
